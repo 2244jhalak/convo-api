d3426edeaba02b26867040872b866c51f4235852
Nakib Hasan Jhalak <stdjhalak@gmail.com>
2025-11-20 15:19:24 +0600

    first commit

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,14 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+
+    name: string;
+    email: string;
+    password: string;
+    role: "user" | "admin";
+    friends: string[];
+    profileImage?: string;
+    createdAt: Date;
+    updatedAt: Date
+
+}
---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const UserSchema= new Schema<IUser> (
+
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ["user", "admin"], default: "user" },
+        friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        profileImage: { type: String },
+    },
+    { timestamps: true }
+)
+
+export const UserModel= mongoose.model("User", UserSchema)
